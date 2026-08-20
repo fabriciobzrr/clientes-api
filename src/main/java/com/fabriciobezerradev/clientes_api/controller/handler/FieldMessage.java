002467e95699ce9389381f8b76db6ec840834208
Fabricio Bezerra <fabriciofrontdev@gmail.com>
2026-08-20 13:01:15 -0300

    Estrutura de pastas

--- src/main/java/com/fabriciobezerradev/clientes_api/controller/handler/FieldMessage.java
+++ src/main/java/com/fabriciobezerradev/clientes_api/controller/handler/FieldMessage.java
@@ -1,4 +1,4 @@
-package com.fabriciobezerradev.clientes_api.controller.exceptions;
+package com.fabriciobezerradev.clientes_api.controller.handler;
 
 public class FieldMessage {
     private String fieldName;
@@ -13,7 +13,7 @@ public class FieldMessage {
         return fieldName;
     }
 
-    public String getMessage() {
+    public String getMessage() {    
         return message;
     }
 
